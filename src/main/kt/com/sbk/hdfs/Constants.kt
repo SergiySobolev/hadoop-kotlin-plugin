abc05a8f31379d702bb3c1db91a3e3b8c63893f6
hadoop <sergiy.sobolev@gmail.com>
2019-12-15 20:50:38 +0200

    SequenceFile write demo

--- src/main/kt/com/sbk/hdfs/Constants.kt
+++ src/main/kt/com/sbk/hdfs/Constants.kt
@@ -4,6 +4,7 @@ package com.sbk.hdfs
 object Constants {
     const val QUANGLE_FILE_URI = "hdfs://localhost/user/hadoop/quangle.txt"
     const val USER_FOLDER_URI = "hdfs://localhost/user/hadoop/"
+    const val DFS_URI = "hdfs://localhost/"
     const val BUFFER_SIZE = 4096
     const val BIG_TEXT_FILE = "/home/hadoop/hadoop-data/test/bigtextfile.txt"
 }
---
+++ src/main/kt/com/sbk/sequencefile/SequenceFileWriteDemo.kt
@@ -0,0 +1,59 @@
+package com.sbk.sequencefile
+
+import com.sbk.hdfs.Constants.DFS_URI
+import com.sbk.hdfs.Constants.USER_FOLDER_URI
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.FileSystem
+import org.apache.hadoop.fs.Path
+import org.apache.hadoop.io.IntWritable
+import org.apache.hadoop.io.SequenceFile
+import org.apache.hadoop.io.SequenceFile.Writer
+import org.apache.hadoop.io.Text
+import java.net.URI
+
+class SequenceFileWriteDemo {
+
+    private val DATA = arrayOf(
+            "One, two, buckle my shoe",
+            "Three, four, shut the door",
+            "Five, six, pick up sticks",
+            "Seven, eight, lay them straight",
+            "Nine, ten, a big fat hen"
+    )
+
+    private val FILE_NAME = "numbers2.seq"
+
+    fun createSequenceFile() {
+        val uri = USER_FOLDER_URI + FILE_NAME
+        val conf = Configuration()
+        val fs = FileSystem.get(URI.create(DFS_URI), conf)
+        val path = Path(uri)
+
+        val writer = SequenceFile.createWriter(
+                conf,
+                Writer.stream(fs.create(path)),
+                Writer.keyClass(IntWritable::class.java),
+                Writer.valueClass(Text::class.java)
+        )
+        writer.use { w -> writeDataToFile(w) }
+
+    }
+
+    private fun writeDataToFile(w: Writer) {
+        val key = IntWritable()
+        val value = Text()
+        for (i in 0..99) {
+            key.set(100 - i)
+            value.set(DATA[i % DATA.size])
+            println("[%s]\t%s\t%s".format(w.length, key, value))
+            w.append(key, value)
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SequenceFileWriteDemo().createSequenceFile()
+        }
+    }
+}
